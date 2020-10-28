10980a55d814924df4a6f6ac2aee5780340f03fc
Yasmim Barbosa Vieira <yasmim.vieira@aluno.faculdadeimpacta.com.br>
2020-10-28 01:06:12 -0300

    chore: condicionais

---
+++ condicionais.js
@@ -0,0 +1,37 @@
+console.log("Criando listas")
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+const idadeComprador = 15;
+const estaAcompanhada = true;
+const temPassagemComprada = true;
+
+console.log("Destinos possíveis: ");
+console.log(listaDeDestinos);
+
+if (idadeComprador >= 18 || estaAcompanhada == true) {
+    console.log("Boa viagem!");
+    listaDeDestinos.splice(1, 1); //retira um elemento da lista
+} else {
+    console.log("Não é maior de idade e não posso vender");
+}
+
+console.log("Embarque: \n\n")
+if (idadeComprador >= 18 && temPassagemComprada) {
+    console.log("Boa viagem");
+} else {
+    console.log("Você não pode embarcar");
+}
+
+console.log(listaDeDestinos);
+
+//operadores logicos
+console.log(idadeComprador > 19)
+console.log(idadeComprador < 19)
+console.log(idadeComprador <= 19)
+console.log(idadeComprador >= 19)
+console.log(idadeComprador == 19)
